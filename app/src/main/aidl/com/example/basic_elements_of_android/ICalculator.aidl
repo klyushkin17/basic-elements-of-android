b5378038456e5cd7cb45a11a3ea97feccf6e913f
klyushkin <stepan.9h88@mail.ru>
2024-08-16 23:56:17 +0300

    Create service in new process

---
+++ app/src/main/aidl/com/example/basic_elements_of_android/ICalculator.aidl
@@ -0,0 +1,9 @@
+// ICalculator.aidl
+package com.example.basic_elements_of_android;
+
+// Declare any non-default types here with import statements
+
+interface ICalculator {
+
+    int add (int a, int b);
+}
---
+++ app/src/main/java/com/example/basic_elements_of_android/services/MyService.kt
@@ -0,0 +1,21 @@
+package com.example.basic_elements_of_android.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+
+class MyService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return
+    }
+
+    private val calculator = object: ICalculator.Stub() {
+
+        override fun add(a: Int, b: Int): Int {
+            ProcessHelper.logProcessName(this)
+            ProcessHelper.logThreadName(this)
+            return a + b
+        }
+    }
+}
